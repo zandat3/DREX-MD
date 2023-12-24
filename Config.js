ea8e0f38d8b87102420ae14804a4434f9f7ae94c
zandat3 <133093258+zandat3@users.noreply.github.com>
2023-12-24 07:34:56 +0300

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,16 @@
+{
+    "ownerNumber": ["254102074064@s.whatsapp.net"],
+    "ownername": "𝐃𝐑𝐄𝐗",
+    "Instagram" : "https://instagram.com/drex_mose",
+    "WhatsApp group" : "https://chat.whatsapp.com/IH7qSawnt5x5iAR9RGg8C6",
+    "botname": "𝐃𝐑𝐄𝐗 𝐁𝐎𝐓",
+    "author": "©𝐝𝐫𝐞𝐱_𝐦𝐨𝐬𝐞",
+    "kwntl": "https://telegra.ph/file/fecf53c0b70fd1018bf3e.jpg",
+    "gametime": 90,
+    "limitCount": 20,
+    "gcount": {
+        "prem": 1000,
+        "user": 15
+    }
+      }
+  
