79ca31ad857c2ebb7a457941e6c27a14f1127928
zandat3 <133093258+zandat3@users.noreply.github.com>
2023-12-24 08:49:16 +0300

    Create setup guide.js

---
+++ setup guide.js
@@ -0,0 +1,39 @@
+THIS SCRIPT WAS MADE BY TAAOFC USING A BASE FROM HW MOD
+
+========================================================
+DON'T DELETE MY CREDITS, DOG, WHATEVER
+ALREADY REMOVED THE CREDITS AND THEN SELLING IT TO A CHILD
+DICK.
+========================================================
+
+
+#####🗿
+HOW TO RECODE
+
+1 HOW TO RECODE MENU TEXT
+
+- GO TO FOLDER lib/taamenu.js
+
+2. HOW TO RECODE OWNER
+
+- LOG IN TO okaw.js CHANGE YOUR NUMBER
+- GO TO owner.json CHANGE YOUR NUMBER
+
+3. HOW TO RECODE IMAGES
+
+- GO TO baseoka/image CHANGE IT ACCORDING TO YOUR WISHES
+- GO TO oka.js SEARCH IN LINER 234 YOU LOOK FOR const reply IF YOU HAVE THE thumbnailUrl YOU CHANGE IT INTO YOUR IMAGE LINK, LIKE THIS IS AN EXAMPLE
+
+ thumbnailUrl : 'Your image link'
+
+-------------------
+CAN'T RECODE? 🗿
+-------------------
+
+JUST USE MY SERVICES🗿
+
+INCASE OF ANY DIFFICULTIES
+WA.ME: +254102074064
+OR EMAIL: darkinteny120@gmail.com
+
+DO NOT SELL THIS SC😡
